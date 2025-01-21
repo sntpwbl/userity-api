6542414895ffbbfec7e5099d3eca57e07452679b
sntpwbl <joaop401@outlook.com>
2025-01-21 16:16:53 -0300

    feat: initial commit

---
+++ src/main/java/com/study/userity/UserityApplication.java
@@ -0,0 +1,13 @@
+package com.study.userity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/erudio_classes/ErudioClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.erudio_classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErudioClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
