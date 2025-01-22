5b0c1abbabcea3a2f6b386ef206dc8b98e86c510
sntpwbl <joaop401@outlook.com>
2025-01-21 23:21:51 -0300

    feat: user model + mapping n:m relationship

---
+++ src/main/java/com/study/userity/model/User.java
@@ -0,0 +1,81 @@
+package com.study.userity.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", unique = true)
+    private String userName;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column
+    private String password;
+    
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
+    
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
+    
+    @Column(name = "credentials_non_expired")
+    private boolean credentialsNonExpired;
+    
+    @Column
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission", joinColumns = {@JoinColumn (name = "id_user")}, inverseJoinColumns = {@JoinColumn (name = "id_permission")})
+    private List<Permission> permissions;
+
+    public List<String> getRoles(){
+        List<String> roles = new ArrayList<>();
+        for(Permission permission : permissions){
+            roles.add(permission.getDescription());
+        }
+        return roles;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+    
+
+}
